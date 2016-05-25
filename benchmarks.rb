ea714dda1680e02bf6b19e3550acc19f0b05b938
Javier <javierhonduco@gmail.com>
2016-05-25 07:12:55 +0300

    added some benchmarks

---
+++ benchmarks.rb
@@ -0,0 +1,26 @@
+require 'benchmark/ips'
+require './fast_sum'
+
+n = 10_000_000
+array = Array.new(n){ rand(0..10_000_000) }
+fast = Fast.new
+
+Benchmark.ips do |x|
+  x.report('r reduce'){
+    array.reduce{|a, b| a+b}
+  }
+  x.report('r classic'){
+    result = 0
+    array.each do |el|
+      result += el
+    end
+  }
+  x.report('r inject'){
+    array.inject(:+)
+  }
+  x.report('c'){
+    fast.sum(array)
+  }
+
+  x.compare!
+end
--- test.rb
+++ test.rb
@@ -1,6 +1,29 @@
-`make`
+require 'minitest/autorun'
 require './fast_sum'
-require 'irb'
 
-IRB.start
-Fast.new.sum([1, 2, 3])
+class FastSumTest < Minitest::Test
+  def setup
+    `make`
+    @fast = Fast.new
+  end
+
+  def test_one_integer
+    assert_equal 2, @fast.sum([2])
+  end
+
+  def test_multiple_integers
+    assert_equal 101, @fast.sum([25, 50, 25, 1])
+  end
+
+  def test_empty_array
+    assert_equal 0, @fast.sum([])
+  end
+
+  def test_not_numeric_input
+    assert_raises(ArgumentError){
+      @fast.sum(:symbol)
+    }
+  end
+end
+
+
